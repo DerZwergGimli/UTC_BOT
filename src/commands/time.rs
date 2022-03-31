ba6a8d51c3e1d2cc2d29313b849f728cb344ba80
DZWG <paule22@protonmail.com>
2022-03-31 23:18:28 +0200

    workable

---
+++ src/commands/time.rs
@@ -0,0 +1,35 @@
+use std::sync::Arc;
+
+use chrono::*;
+use serenity::framework::standard::{Args, CommandResult, macros::command};
+use serenity::http::Http;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use tracing::info;
+
+#[command]
+pub async fn time(ctx: &Context, msg: &Message) -> CommandResult {
+    let time = chrono::offset::Utc::now();
+
+
+    let time_string = format!("TIME IS {:?}", time).to_string();
+
+    msg.channel_id.say(&ctx.http, time_string).await?;
+
+    Ok(())
+}
+
+pub async fn change_display_name(ctx: &Arc<Http>, guilds: &Vec<GuildId>) -> CommandResult {
+    info!("BEEP");
+
+    for guild in guilds {
+        let datetime: DateTime<Utc> = DateTime::from(chrono::Utc::now());
+
+        let time = datetime.format("%d. %b %H:%M UTC");
+        let time_string = format!("{}", time);
+
+
+        guild.edit_nickname(ctx, Some(&*time_string)).await?;
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,139 @@
+//! Requires the 'framework' feature flag be enabled in your project's
+//! `Cargo.toml`.
+//!
+//! This can be enabled by specifying the feature in the dependency section:
+//!
+//! ```toml
+//! [dependencies.serenity]
+//! git = "https://github.com/serenity-rs/serenity.git"
+//! features = ["framework", "standard_framework"]
+//! ```
+use std::{collections::HashSet, env, sync::Arc};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::time::Duration;
+
+use env_logger::Env;
+use log::*;
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::{standard::macros::group, StandardFramework},
+    http::Http,
+    model::{event::ResumedEvent, gateway::Ready},
+    prelude::*,
+};
+use serenity::futures::StreamExt;
+use serenity::model::id::GuildId;
+
+use commands::{math::*, meta::*, owner::*, time::*};
+
+use crate::commands::time;
+
+//use tracing::{error, info};
+
+mod commands;
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct Handler {
+    is_loop_running: AtomicBool,
+}
+
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+
+    async fn cache_ready(&self, ctx: Context, _guilds: Vec<GuildId>) {
+        println!("Cache built successfully!");
+
+        loop {
+            time::change_display_name(&ctx.http,&_guilds).await;
+
+            tokio::time::sleep(Duration::from_secs(120)).await;
+        }
+
+        /*
+                if !self.is_loop_running.load(Ordering::Relaxed) {
+                    let ctx1 = Arc::clone(&ctx);
+                    tokio::spawn(async move {
+                        loop {
+                            time::change_display_name(Arc::clone(&ctx1)).await;
+
+                            //log_system_load(Arc::clone(&ctx1)).await;
+                            info!("Pooping");
+
+                            tokio::time::sleep(Duration::from_secs(120)).await;
+                        }
+                    });
+
+                    self.is_loop_running.swap(true, Ordering::Relaxed);
+                }*/
+    }
+}
+
+#[group]
+#[commands(multiply, ping, quit, time)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    let env = Env::default().filter_or("LOG_LEVEL", "trace");
+
+    env_logger::init_from_env(env);
+    info!("INIT LOGGER!");
+
+
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    // Create the framework
+    let framework =
+        StandardFramework::new().configure(|c| c.owners(owners).prefix("+")).group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(&token)
+        .framework(framework)
+        .event_handler(Handler {
+            is_loop_running: AtomicBool::new(false),
+        })
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
